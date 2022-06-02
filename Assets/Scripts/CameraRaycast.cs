eb495705c5d69ca7f859d8da97ac4560797d7d67
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2022-06-02 19:15:43 -0400

    Color change back
    Figured out how to change color back from raycast.

--- Assets/Scripts/CameraRaycast.cs
+++ Assets/Scripts/CameraRaycast.cs
@@ -3,22 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRaycast : MonoBehaviour
-{
-    bool prevHit;
+{   
+    // A boolean value that tells us whether or not we have hit something yet, prevents us from NullRef exceptioning at the start of our code
+    bool hitYet;
+
+    // A gameobject to hold our last gameobject hit by a raycast
+    private GameObject lastObject;
 
     private void FixedUpdate()
     {
-        // hit variable to store result
         RaycastHit hit;
 
-        // a ray going from "screen coordinates" forward into our game world
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-
-        // Conduct our Raycast
-        if (Physics.Raycast(ray, out hit))
+        if (hitYet)
         {
-            hit.collider.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+            if (Physics.Raycast(ray, out hit))
+            {
+                hitYet = true;
+                if (hitObjectGetter(ray, hit) != null)
+                {
+                    lastObject = hit.collider.gameObject;
+
+                    Debug.Log("Now touching " + lastObject);
+
+                   lastObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+                }
+            }
+            else
+            {
+                Debug.Log("No longer touching " + lastObject);
+                lastObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+            }
+        }
+        else
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                hitYet = true;
+                lastObject = hit.collider.gameObject;
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.yellow);
+            }
+        }
+    }
+
+    private GameObject hitObjectGetter(Ray aRay, RaycastHit aHit)
+    {
+        if(Physics.Raycast(aRay, out aHit))
+        {
+            return aHit.collider.gameObject;
+        }
+        else
+        {
+            return null;
         }
     }
 }
